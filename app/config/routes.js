696af8c389c6a6e4a08d390a976a1bed422ade6d
Muhammet <muhammeta7@gmail.com>
2017-03-01 10:38:50 -0500

    minor changes to routes.js in config directory

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,23 +1,40 @@
+// Inclue the React library
 var React = require("react");
+
+// Include the react-router module
 var router = require("react-router");
 
+// Include the Route component
 var Route = router.Route;
 
+//  Include the IndexRoute (catch-all route)
 var IndexRoute = router.IndexRoute;
+
+// Include the Router component
 var Router = router.Router;
 
+// Include the browserHistory prop to configure client side routing
 var browserHistory = router.browserHistory;
 
-var Main require("../components/Main");
+// Reference the high-level components
+var Main = require("../components/Main");
 var Search = require("../components/Search");
 var Saved = require("../components/Saved");
 
+
+// Export the Routes
 module.exports = (
+  // High level component is the Router component.
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
+
+      {/* If user selects Search or Saved show the appropriate component */}
       <Route path="Search" component={Search} />
       <Route path="Saved" component={Saved} />
+
+      {/* If user selects any other path... we get the Home Route */}
       <IndexRoute component={Search} />
+
     </Route>
   </Router>
 );
